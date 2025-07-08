a02e13705b1edf04e94cdb7d50c002ab8d786a32
Misael-R <misaelrmeneses@outlook.com>
2025-07-08 00:37:07 -0600

    Baseline classifier df var typos corrected from ['text', 'label'] to ['Message', 'Label']

--- src/scripts/train_model.py
+++ src/scripts/train_model.py
@@ -4,18 +4,19 @@ import pandas as pd
 import joblib
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
-import sys
-sys.path.insert(0, 'utils')
-from preprocessing import clean_text
+from preprocessing import clean_text, clean_text2
+
+# splits = {'train': 'train.csv', 'test': 'test.csv'}
+# df = pd.read_csv("hf://datasets/adamlouly/enron_spam_data/" + splits["train"])
 
 # Sample or full Enron dataset
-df = pd.read_csv('enron_spam.csv')  # ['text', 'label']
-df['clean_text'] = df['text'].apply(clean_text)
+df = pd.read_csv('../../assets/enronSpamDataset/enron_spam_data.csv')  # ['Message', 'Label']
+df['clean_text'] = df['Message'].apply(clean_text)
 
 # TF-IDF
 vectorizer = TfidfVectorizer(max_features=3000)
 X = vectorizer.fit_transform(df['clean_text'])
-y = df['label']
+y = df['Label']
 
 # Train model
 clf = LogisticRegression()
