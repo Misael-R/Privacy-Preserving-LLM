030a4070b0c7c30ca04c923a9d1a7cfcfdda394d
Misael-R <misaelrmeneses@outlook.com>
2025-07-08 00:36:05 -0600

    Handled non str data types, this will be updated soon for better preprocessing

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -4,5 +4,12 @@ import re
 
 def clean_text(text):
     """Lowercases and removes non-alphanumeric characters from text."""
+    text = str(text)
     text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
+    print(text)
+    return text.lower()
+
+def clean_text2(text):
+    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r'\W+', ' ', text)
     return text.lower()
